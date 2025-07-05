f49a959661513755f452f0225f859a21269ec825
Khanh Tran <tranquanki28@gmail.com>
2025-07-05 10:56:32 +0700

    fix: recover missed file

---
+++ .config/fish/conf.d/alias.fish
@@ -0,0 +1,28 @@
+# Common
+alias vim="nvim"
+alias g="git"
+
+# Tmux
+alias tm="tmux"
+alias ta="tm attach"
+alias tpi="~/.config/tmux/plugins/tpm/bin/install_plugins" # Installs Tmux plugins
+alias tpu="~/.config/tmux/plugins/tpm/bin/update_plugins all" # Updates all Tmux plugins
+
+# Lsd
+alias ls="lsd"
+alias l="ls -l"
+alias la="ls -a"
+alias lla="ls -la"
+alias lt="ls --tree"
+
+# translate shell
+alias t="trans :vi"
+
+# fzf cd
+alias sf "cd (fd -td -H | fzf)"
+alias ff "~/.local/share/bin/fzf-preview.sh"
+
+# image preview
+alias iv "kitten icat"
+
+alias tocchien="scrcpy --max-size 2000 --video-bit-rate 16M --fullscreen"
