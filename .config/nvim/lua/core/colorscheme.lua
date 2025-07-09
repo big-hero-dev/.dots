620af4c1f30b4c625e2173fa41729dc95c31b306
Khanh Tran <tranquanki28@gmail.com>
2025-07-09 10:34:45 +0700

    style: new wallpaper

--- .config/nvim/lua/core/colorscheme.lua
+++ .config/nvim/lua/core/colorscheme.lua
@@ -3,3 +3,12 @@ local add = require("mini.deps").add
 add({ source = "alexxGmZ/e-ink.nvim" })
 
 vim.cmd([[colorscheme e-ink]])
+
+-- Transparent background
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
