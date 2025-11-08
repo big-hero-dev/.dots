d46eb7e365c856641168315fd388bd07b208a7e8
Khanh Tran <tranquanki28@gmail.com>
2025-11-08 19:48:04 +0700

    style: scrspy

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -25,4 +25,4 @@ alias ff "~/.local/share/bin/fzf-preview.sh"
 # image preview
 alias iv "kitten icat"
 
-alias lol="scrcpy --max-size 2000 --video-bit-rate 16M --turn-screen-off --fullscreen"
+alias lol="scrcpy --max-size 2000 --video-bit-rate 16M --turn-screen-off"
