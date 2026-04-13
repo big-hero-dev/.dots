68c4518cb965e4e67a115ad29679fc7c95b6383e
Khanh Tran <tranquanki28@gmail.com>
2026-04-13 08:44:06 +0700

    neovim: group tiny-cmd

--- .config/nvim/plugin/01-plugins.lua
+++ .config/nvim/plugin/01-plugins.lua
@@ -7,9 +7,6 @@ vim.pack.add({
 	"https://github.com/lambdalisue/suda.vim",
 	"https://github.com/akinsho/toggleterm.nvim",
 	"https://github.com/folke/zen-mode.nvim",
-})
-
-vim.pack.add({
 	"https://github.com/rachartier/tiny-cmdline.nvim",
 })
 
