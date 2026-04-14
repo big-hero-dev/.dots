4b5651604dd35c5c06fceba92abeb200a83bc1ee
Khanh Tran <tranquanki28@gmail.com>
2026-04-14 14:55:41 +0700

    neovim: prettier

--- .config/nvim/plugin/conform.lua
+++ .config/nvim/plugin/conform.lua
@@ -1,12 +1,12 @@
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
-		javascript = { "prettierd" },
-		typescript = { "prettierd" },
-		javascriptreact = { "prettierd" },
-		typescriptreact = { "prettierd" },
-		html = { "prettierd" },
-		css = { "prettierd" },
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		html = { "prettier" },
+		css = { "prettier" },
 		php = { "php-cs-fixer" },
 	},
 	formatters = {
