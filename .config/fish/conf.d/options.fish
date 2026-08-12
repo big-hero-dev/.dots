b2eb450c5ba84ebbfbe8692b3df94c3aa98ed90b
Khanh Tran <codebymeow@gmail.com>
2026-08-12 08:45:22 +0700

    fix: cursor blink

--- .config/fish/conf.d/options.fish
+++ .config/fish/conf.d/options.fish
@@ -4,3 +4,6 @@ set -Ux VISUAL nvim
 
 fish_add_path -g ~/.node-modules/ ~/.node-modules/bin ~/.node-modules/ /share/man ~/.cargo/bin
 
+function set_cursor_blink --on-event fish_prompt
+    printf '\e[1 q'
+end
